1adee88f2aca415cfa472c15c7f4892446d9aaf8
faux123 <reioux@gmail.com>
2011-05-22 10:30:51 -0700

    BFS 4.0.4 Experimental Patch

--- kernel/sched.c
+++ kernel/sched.c
@@ -1,3 +1,6 @@
+#ifdef CONFIG_SCHED_BFS
+#include "sched_bfs.c"
+#else
 /*
  *  kernel/sched.c
  *
@@ -11220,3 +11223,4 @@ void synchronize_sched_expedited(void)
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
 
 #endif /* #else #ifndef CONFIG_SMP */
+#endif /* CONFIG_SCHED_BFS */
