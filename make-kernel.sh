2948fb4fec66df01e01d25c07070703022223590
Travis Tabbal <travis@tabbal.net>
2011-08-15 15:52:20 -0600

    Added zcache support and missing bit from furure kernels.

--- make-kernel.sh
+++ make-kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CROSS_COMPILE=/android-ndk-r6/toolchains/arm-linux-androideabi-4.4.3/prebuilt/darwin-x86/bin/arm-linux-androideabi-
-export CFLAGS=""
-export LDFLAGS=""
+export CFLAGS=
+export LDFLAGS=
 
 make ARCH=arm -j8
